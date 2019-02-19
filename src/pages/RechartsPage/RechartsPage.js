d60b95f2f3a3f953f1fbc19ccba7fb80eacd4ffa
Sergiu <sergiu.munteanu@live.com>
2019-02-19 17:10:29 +0100

    Added some styles and comments on recharts example

--- src/pages/RechartsPage/RechartsPage.js
+++ src/pages/RechartsPage/RechartsPage.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import {
-    ResponsiveContainer, ComposedChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+    ResponsiveContainer, ComposedChart, Line, XAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 import data from './data'
 
@@ -9,7 +9,9 @@ export default class RechartsPage extends PureComponent {
     render() {
         return (
             <div>
-                <div style={{ width: '100%', height: 300 }}>
+                <h2>Recharts</h2>
+                <br />
+                <div style={{ width: '100%', height: 500 }}>
                     <ResponsiveContainer>
                         <ComposedChart
                             width={500}
@@ -19,16 +21,28 @@ export default class RechartsPage extends PureComponent {
                                 top: 20, right: 20, bottom: 20, left: 20,
                             }}
                         >
-                            <CartesianGrid stroke="#f5f5f5" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
+                            <CartesianGrid horizontal={ false } stroke="#f5f5f5" />
+                            <XAxis
+                                dataKey="name"
+                                minTickGap={ 20 }
+                                ticksCount={ 6 }  />
+                            <Tooltip cursor={{ stroke: '#ccc', strokeWidth: 1 }}  />
                             <Legend />
                             <Line type="monotone" dataKey="2018" stroke="#fe4e1e" />
                             <Line type="monotone" dataKey="2019" stroke="#d2d2d2" />
                         </ComposedChart>
                     </ResponsiveContainer>
                 </div>
+                <br />
+                <p><strong>Notes:</strong></p>
+                <ul>
+                    <li><a href="https://github.com/jsergiu/testing-graph-libraries/blob/master/src/pages/RechartsPage/RechartsPage.js" target="_blank">Source code</a></li>
+                    <li>Nice that is composable. You can create a styled yAxis component and reuse it in multiple charts;</li>
+                    <li>Issue: I set ticksCount=6 but it displays more than 6 ticks;</li>
+                    <li>Issue: sometimes the vertical lines in the grid and the labels are missaligned</li>
+                    <li>Works well on all browsers. Minor issues on android</li>
+                </ul>
+                <br/>
             </div>
         );
     }
